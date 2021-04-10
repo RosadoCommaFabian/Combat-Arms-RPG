1435b5c42146c1c9faba549228e1b87170524950
Skorps123 <fprosado0@gmail.com>
2021-04-10 16:28:28 -0500

    Added Weapon class and ASCII art

---
+++ CombatArmsRPG/CombatArmsRPG/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CombatArmsRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" .o88b.  .d88b.  .88b  d88. d8888b.  .d8b.  d888888b    .d8b.  d8888b. .88b  d88. .d8888.\n" +
+                              "d8P  Y8 .8P  Y8. 88'YbdP`88 88  `8D d8' `8b `~~88~~'   d8' `8b 88  `8D 88'YbdP`88 88'  YP\n" +
+                              "8P      88    88 88  88  88 88oooY' 88ooo88    88      88ooo88 88oobY' 88  88  88 `8bo.  \n" +
+                              "8b      88    88 88  88  88 88~~~b. 88~~~88    88      88~~~88 88`8b   88  88  88   `Y8b.\n" +
+                              "Y8b  d8 `8b  d8' 88  88  88 88   8D 88   88    88      88   88 88 `88. 88  88  88 db   8D\n" +
+                              " `Y88P'  `Y88P'  YP  YP  YP Y8888P' YP   YP    YP      YP   YP 88   YD YP  YP  YP `8888Y'\n");
+        }
+    }
+}
---
+++ CombatArmsRPG/CombatArmsRPG/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CombatArmsRPG
+{
+    public class Weapon
+    {
+        public string Name { get; set; }
+        public double Damage { get; set; }
+        public int Ammo { get; set; }
+        public double MissChance { get; set; }
+        public int Range { get; set; }
+        public int Cost { get; set; }
+        public LoudoutSlot Slot { get; set; }
+
+        public Weapon(string name, double damage, int ammo, double missChance, int range, int cost, LoudoutSlot slot)
+        {
+            Name = name;
+            Damage = damage;
+            Ammo = ammo;
+            MissChance = missChance;
+            Range = range;
+            Cost = cost;
+            Slot = slot;
+        }
+
+        public enum LoudoutSlot
+        {
+            PrimaryWeapon,
+            SecondaryWeapon,
+            MeleeWeapon,
+            SupportWeapon
+        }
+    }
+}
